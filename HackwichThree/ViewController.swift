afe529819f690e2ee18d0215d34ea8d0d28585ef
clarkcuadro <clarkant@hawaii.edu>
2018-02-05 10:47:09 -1000

    added glyph

--- HackwichThree/ViewController.swift
+++ HackwichThree/ViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var firstLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+    //set firstlabel tothe string "About"
+    self.firstLabel.text = "About"
+    
+    
+    
     }
 
     override func didReceiveMemoryWarning() {
