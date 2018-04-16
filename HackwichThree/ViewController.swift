fb926563d180e4ec7c62ff5e0a5c1d1e15a09584
CM Student <cmstudent@uhwo-acm-09.local>
2018-04-16 12:14:54 -1000

    finished tab 1

--- HackwichThree/ViewController.swift
+++ HackwichThree/ViewController.swift
@@ -10,16 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet var firstLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var majoris: UILabel!
+    @IBOutlet weak var majorDescription: UILabel!
+    @IBOutlet weak var mySpring: UILabel!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var logoImage: UIImageView!
+    @IBOutlet weak var pictureImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     
     //set firstlabel tothe string "About"
-    self.firstLabel.text = "About"
-    
-    
-    
+        self.firstLabel.text = "My Name is:"
+        self.nameLabel.text = "Clark"
+        self.majoris.text = "My Major is:"
+        self.majorDescription.text = "ACM"
+        self.mySpring.text = "My Spring 2018 Classes are:"
+        self.textView.text = "I am a new student to UH West Oahu but not the UH system.  I'm semi-retired balancing military and passion for arts.  I chose the infinite creativeness of the Arts to live my dream. Currently in three classes.  In CM 161 and it's awesome!"
+        self.pictureImage.image = UIImage(named:"superman.jpeg")
+        self.logoImage.image = UIImage(named: "uhwo.png")
     }
 
     override func didReceiveMemoryWarning() {
