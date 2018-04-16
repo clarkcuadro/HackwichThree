c878c2701e6ee6898e63d670d68f46f219f6f3e5
CM Student <cmstudent@uhwo-acm-09.local>
2018-04-16 12:27:32 -1000

    tableView connections

--- HackwichThree/ViewControllerTwoViewController.swift
+++ HackwichThree/ViewControllerTwoViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewControllerTwoViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
